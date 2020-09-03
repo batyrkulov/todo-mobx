4d21b1b8ffd9cbee309526742947b169436b4fa4
Aidos Batyrkulov <aidosbatyrkulov@gmail.com>
2020-09-03 09:41:21 +0600

    Updates

---
+++ precache-manifest.82363e647068957112c031dfad5392d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "299b4fd418156a2d9376fbf75d963497",
+    "url": "/todo-mobx/index.html"
+  },
+  {
+    "revision": "020f37651a137cfeb3ac",
+    "url": "/todo-mobx/static/css/main.40f78519.chunk.css"
+  },
+  {
+    "revision": "f0f48a799710413fa927",
+    "url": "/todo-mobx/static/js/2.f5c95866.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/todo-mobx/static/js/2.f5c95866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "020f37651a137cfeb3ac",
+    "url": "/todo-mobx/static/js/main.6e540114.chunk.js"
+  },
+  {
+    "revision": "6af9f6e10c206215ae13",
+    "url": "/todo-mobx/static/js/runtime-main.712f9e1e.js"
+  }
+]);
